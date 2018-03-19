9bb508686d9b78325cefd72dd85804426825ef73
Erik Unger <erik@erikunger.com>
2018-03-19 14:31:34 +0100

    renamed  DispatchCombined to DispatchCombinedCommandAndArgs

--- stringargsdispatcher.go
+++ stringargsdispatcher.go
@@ -32,11 +32,14 @@ type stringArgsCommand struct {
 }
 
 func checkCommandChars(command string) error {
-	if strings.IndexFunc(command, unicode.IsSpace) != -1 {
+	if strings.IndexFunc(command, unicode.IsSpace) >= 0 {
 		return errors.Errorf("Command contains space characters: '%s'", command)
 	}
 	if strings.IndexFunc(command, unicode.IsGraphic) == -1 {
 		return errors.Errorf("Command contains non graphc characters: '%s'", command)
+	}
+	if strings.ContainsAny(command, "|&;()<>") {
+		return errors.Errorf("Command contains invalid characters: '%s'", command)
 	}
 	return nil
 }
@@ -63,7 +66,7 @@ func NewStringArgsDispatcher(loggers ...StringArgsCommandLogger) *StringArgsDisp
 	}
 }
 
-func (disp StringArgsDispatcher) AddCommand(command, description string, commandFunc interface{}, args Args, resultsHandlers ...ResultsHandler) error {
+func (disp *StringArgsDispatcher) AddCommand(command, description string, commandFunc interface{}, args Args, resultsHandlers ...ResultsHandler) error {
 	if err := checkCommandChars(command); err != nil {
 		return errors.Wrapf(err, "Command '%s'", command)
 	}
@@ -82,14 +85,14 @@ func (disp StringArgsDispatcher) AddCommand(command, description string, command
 	return nil
 }
 
-func (disp StringArgsDispatcher) MustAddCommand(command, description string, commandFunc interface{}, args Args, resultsHandlers ...ResultsHandler) {
+func (disp *StringArgsDispatcher) MustAddCommand(command, description string, commandFunc interface{}, args Args, resultsHandlers ...ResultsHandler) {
 	err := disp.AddCommand(command, description, commandFunc, args, resultsHandlers...)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func (disp StringArgsDispatcher) AddDefaultCommand(description string, commandFunc interface{}, args Args, resultsHandlers ...ResultsHandler) error {
+func (disp *StringArgsDispatcher) AddDefaultCommand(description string, commandFunc interface{}, args Args, resultsHandlers ...ResultsHandler) error {
 	stringArgsFunc, err := GetStringArgsFunc(commandFunc, args, resultsHandlers...)
 	if err != nil {
 		return errors.Wrap(err, "Default command")
@@ -105,24 +108,24 @@ func (disp StringArgsDispatcher) AddDefaultCommand(description string, commandFu
 	return nil
 }
 
-func (disp StringArgsDispatcher) MustAddDefaultCommand(description string, commandFunc interface{}, args Args, resultsHandlers ...ResultsHandler) {
+func (disp *StringArgsDispatcher) MustAddDefaultCommand(description string, commandFunc interface{}, args Args, resultsHandlers ...ResultsHandler) {
 	err := disp.AddDefaultCommand(description, commandFunc, args, resultsHandlers...)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func (disp StringArgsDispatcher) HasCommnd(command string) bool {
+func (disp *StringArgsDispatcher) HasCommnd(command string) bool {
 	_, found := disp.comm[command]
 	return found
 }
 
-func (disp StringArgsDispatcher) HasDefaultCommnd() bool {
+func (disp *StringArgsDispatcher) HasDefaultCommnd() bool {
 	_, found := disp.comm[Default]
 	return found
 }
 
-func (disp StringArgsDispatcher) Dispatch(command string, args ...string) error {
+func (disp *StringArgsDispatcher) Dispatch(command string, args ...string) error {
 	cmd, found := disp.comm[command]
 	if !found {
 		return ErrNotFound
@@ -133,25 +136,25 @@ func (disp StringArgsDispatcher) Dispatch(command string, args ...string) error 
 	return cmd.stringArgsFunc(args...)
 }
 
-func (disp StringArgsDispatcher) MustDispatch(command string, args ...string) {
+func (disp *StringArgsDispatcher) MustDispatch(command string, args ...string) {
 	err := disp.Dispatch(command, args...)
 	if err != nil {
 		panic(errors.Wrapf(err, "Command '%s'", command))
 	}
 }
 
-func (disp StringArgsDispatcher) DispatchDefaultCommand() error {
+func (disp *StringArgsDispatcher) DispatchDefaultCommand() error {
 	return disp.Dispatch(Default)
 }
 
-func (disp StringArgsDispatcher) MustDispatchDefaultCommand() {
+func (disp *StringArgsDispatcher) MustDispatchDefaultCommand() {
 	err := disp.DispatchDefaultCommand()
 	if err != nil {
 		panic(errors.Wrap(err, "Default command"))
 	}
 }
 
-func (disp StringArgsDispatcher) DispatchCombined(commandAndArgs []string) (command string, err error) {
+func (disp *StringArgsDispatcher) DispatchCombinedCommandAndArgs(commandAndArgs []string) (command string, err error) {
 	if len(commandAndArgs) == 0 {
 		return Default, disp.DispatchDefaultCommand()
 	}
@@ -160,15 +163,15 @@ func (disp StringArgsDispatcher) DispatchCombined(commandAndArgs []string) (comm
 	return command, disp.Dispatch(command, args...)
 }
 
-func (disp StringArgsDispatcher) MustDispatchCombined(commandAndArgs []string) (command string) {
-	command, err := disp.DispatchCombined(commandAndArgs)
+func (disp *StringArgsDispatcher) MustDispatchCombinedCommandAndArgs(commandAndArgs []string) (command string) {
+	command, err := disp.DispatchCombinedCommandAndArgs(commandAndArgs)
 	if err != nil {
 		panic(err)
 	}
 	return command
 }
 
-func (disp StringArgsDispatcher) PrintCommands(appName string) {
+func (disp *StringArgsDispatcher) PrintCommands(appName string) {
 	list := make([]*stringArgsCommand, 0, len(disp.comm))
 	for _, cmd := range disp.comm {
 		list = append(list, cmd)
@@ -197,7 +200,7 @@ func (disp StringArgsDispatcher) PrintCommands(appName string) {
 	}
 }
 
-func (disp StringArgsDispatcher) PrintCommandsUsageIntro(appName string, output io.Writer) {
+func (disp *StringArgsDispatcher) PrintCommandsUsageIntro(appName string, output io.Writer) {
 	if len(disp.comm) > 0 {
 		fmt.Fprint(output, "Commands:\n")
 		disp.PrintCommands(appName)
