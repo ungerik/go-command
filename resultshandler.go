bf476e4a49872f169edcc1e18f9462094dec5164
Erik Unger <erik@erikunger.com>
2018-06-19 13:59:40 +0200

    call Println per result

--- resultshandler.go
+++ resultshandler.go
@@ -44,6 +44,7 @@ func resultsToInterfaces(results []reflect.Value) ([]interface{}, error) {
 	return r, nil
 }
 
+// PrintTo calls fmt.Fprint on writer with the result values as varidic arguments
 func PrintTo(writer io.Writer) ResultsHandlerFunc {
 	return func(args Args, argVals, resultVals []reflect.Value, resultErr error) error {
 		if resultErr != nil {
@@ -58,95 +59,121 @@ func PrintTo(writer io.Writer) ResultsHandlerFunc {
 	}
 }
 
+// PrintlnTo calls fmt.Fprintln on writer for every result
 func PrintlnTo(writer io.Writer) ResultsHandlerFunc {
 	return func(args Args, argVals, resultVals []reflect.Value, resultErr error) error {
 		if resultErr != nil {
 			return resultErr
 		}
-		r, err := resultsToInterfaces(resultVals)
-		if err != nil || len(r) == 0 {
+		results, err := resultsToInterfaces(resultVals)
+		if err != nil || len(results) == 0 {
 			return err
 		}
-		_, err = fmt.Fprintln(writer, r...)
-		return err
+		for _, r := range results {
+			_, err = fmt.Fprintln(writer, r)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
 	}
 }
 
+// Println calls fmt.Println for every result
 var Println ResultsHandlerFunc = func(args Args, argVals, resultVals []reflect.Value, resultErr error) error {
 	if resultErr != nil {
 		return resultErr
 	}
-	r, err := resultsToInterfaces(resultVals)
-	if err != nil || len(r) == 0 {
+	results, err := resultsToInterfaces(resultVals)
+	if err != nil || len(results) == 0 {
 		return err
 	}
-	_, err = fmt.Println(r...)
-	return err
+	for _, r := range results {
+		_, err = fmt.Println(r)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
+// PrintlnWithPrefixTo calls fmt.Fprintln(writer, prefix, result) for every result value
 func PrintlnWithPrefixTo(prefix string, writer io.Writer) ResultsHandlerFunc {
 	return func(args Args, argVals, resultVals []reflect.Value, resultErr error) error {
 		if resultErr != nil {
 			return resultErr
 		}
-		r, err := resultsToInterfaces(resultVals)
-		if err != nil {
+		results, err := resultsToInterfaces(resultVals)
+		if err != nil || len(results) == 0 {
 			return err
 		}
-		r = append([]interface{}{prefix}, r...)
-		_, err = fmt.Fprintln(writer, r...)
-		return err
+		for _, result := range results {
+			_, err = fmt.Fprintln(writer, prefix, result)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
 	}
 }
 
+// PrintlnWithPrefix calls fmt.Println(prefix, result) for every result value
 func PrintlnWithPrefix(prefix string) ResultsHandlerFunc {
 	return func(args Args, argVals, resultVals []reflect.Value, resultErr error) error {
 		if resultErr != nil {
 			return resultErr
 		}
-		r, err := resultsToInterfaces(resultVals)
-		if err != nil {
+		results, err := resultsToInterfaces(resultVals)
+		if err != nil || len(results) == 0 {
 			return err
 		}
-		r = append([]interface{}{prefix}, r...)
-		_, err = fmt.Println(r...)
-		return err
+		for _, result := range results {
+			_, err = fmt.Println(prefix, result)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
 	}
 }
 
+// Logger interface
 type Logger interface {
 	Printf(format string, args ...interface{})
 }
 
+// LogTo calls logger.Printf(fmt.Sprintln(results...))
 func LogTo(logger Logger) ResultsHandlerFunc {
 	return func(args Args, argVals, resultVals []reflect.Value, resultErr error) error {
 		if resultErr != nil {
 			return resultErr
 		}
-		r, err := resultsToInterfaces(resultVals)
-		if err != nil || len(r) == 0 {
+		results, err := resultsToInterfaces(resultVals)
+		if err != nil || len(results) == 0 {
 			return err
 		}
-		logger.Printf(fmt.Sprintln(r...))
+		logger.Printf(fmt.Sprintln(results...))
 		return nil
 	}
 }
 
+// LogWithPrefixTo calls logger.Printf(fmt.Sprintln(results...)) with prefix prepended to the results
 func LogWithPrefixTo(prefix string, logger Logger) ResultsHandlerFunc {
 	return func(args Args, argVals, resultVals []reflect.Value, resultErr error) error {
 		if resultErr != nil {
 			return resultErr
 		}
-		r, err := resultsToInterfaces(resultVals)
-		if err != nil || len(r) == 0 {
+		results, err := resultsToInterfaces(resultVals)
+		if err != nil || len(results) == 0 {
 			return err
 		}
-		r = append([]interface{}{prefix}, r...)
-		logger.Printf(fmt.Sprintln(r...))
+		results = append([]interface{}{prefix}, results...)
+		logger.Printf(fmt.Sprintln(results...))
 		return nil
 	}
 }
 
+// PrintlnText prints a fixed string if a command returns without an error
 type PrintlnText string
 
 func (t PrintlnText) HandleResults(args Args, argVals, resultVals []reflect.Value, resultErr error) error {
