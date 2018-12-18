7250c75d874d74118ddb7fc467c89cdd1bba6618
Erik Unger <erik@erikunger.com>
2018-12-18 17:09:05 +0100

    remove debug println

--- gorillamux/gorillamux.go
+++ gorillamux/gorillamux.go
@@ -88,7 +88,6 @@ func handleErr(err error, writer http.ResponseWriter, request *http.Request, err
 		return
 	}
 	if len(errHandlers) == 0 {
-		fmt.Println(3)
 		DefaultErrorHandler.HandleError(err, writer, request)
 	} else {
 		for _, errHandler := range errHandlers {
